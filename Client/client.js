5ca690568324dad938f4b260c88c886c2b1cd9df
vilo0001 <vilo0001@stud.kea.dk>
2024-12-09 12:01:51 +0100

    Scroll console log

--- Client/client.js
+++ Client/client.js
@@ -1,1 +1,13 @@
-console.log("test")
+const scrollElementDOM = document.querySelector("#scroll-element");
+
+
+window.addEventListener("wheel", () => {
+    if(event.deltaY < 0) {
+        scrollElementDOM.style.top = scrollElementDOM.style.top + 5;
+        console.log("Scroll up");
+    }
+    else {
+        console.log(scrollElementDOM.style.backgroundColor);
+        console.log("Scroll down");
+    }
+});
