1ef750b2b8479fab2eecdc536dcbb30b1e8c2dbf
vilo0001 <vilo0001@stud.kea.dk>
2024-12-10 11:47:17 +0100

    Basic HTML, server end points. Display post text on HTML from server.

--- Client/client.js
+++ Client/client.js
@@ -1,13 +1,11 @@
-const scrollElementDOM = document.querySelector("#scroll-element");
+const dataDOM = document.querySelector("#data");
 
-
-window.addEventListener("wheel", () => {
-    if(event.deltaY < 0) {
-        scrollElementDOM.style.top = scrollElementDOM.style.top + 5;
-        console.log("Scroll up");
-    }
-    else {
-        console.log(scrollElementDOM.style.backgroundColor);
-        console.log("Scroll down");
-    }
-});
+fetch("http://localhost:3000/test")
+    .then(response => response.json())
+    .then(data => {
+        const dataText = data[0].all_post_text;
+        const paragraphElement = document.createElement("p");
+        const paragraphText = document.createTextNode(dataText);
+        paragraphElement.appendChild(paragraphText);
+        dataDOM.appendChild(paragraphElement);
+    });
--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,49 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('mysql2');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const connection = db.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.dbpassword,
+    database: "facebook"
+});
+
+app.get('/test',(req,res)=> {
+    let q = `SELECT * FROM classification LIMIT 1`;
+    connection.query(q, (error, results)=>{
+        res.send(results);
+    })
+});
+
+app.get('/total-interactions',(req,res)=> {
+    let q = `SELECT * FROM classification LIMIT 1`;
+    connection.query(q, (error, results)=>{
+        res.send(results);
+    })
+});
+
+app.get('/negative-posts',(req,res)=> {
+    let q = `SELECT * FROM classification LIMIT 1`;
+    connection.query(q, (error, results)=>{
+        res.send(results);
+    })
+});
+
+app.get('/economic-support',(req,res)=> {
+    let q = `SELECT * FROM classification LIMIT 1`;
+    connection.query(q, (error, results)=>{
+        res.send(results);
+    })
+});
+
+// Start server. Needs to be below end points.
+app.listen(port, ()=>{
+    console.log("Hey guys we are officially LIVE !!!!");
+});
